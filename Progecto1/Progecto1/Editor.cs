8f431aad24c8ab8d31fcc4592bf0e858bad8bc9d
karenmtz3 <karenmtz273@gmail.com>
2018-08-21 16:03:47 -0500

    Nuevo Proyecto

---
+++ Progecto1/Progecto1/Editor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Progecto1
+{
+    public partial class Editor : Form
+    {
+        
+        /// <summary>
+        /// El constructor de Editor sirve para crear la 
+        /// pestaña en donde se realizaran las acciones a capturar 
+        /// </summary>
+        /// <param name="Nombre">Nombre del proyecto</param>
+        /// <param name="n"> Número de pestaña </param>
+        public Editor(string Nombre, int n)
+        {
+            
+
+            InitializeComponent();
+        }
+
+        private void Editor_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
--- Progecto1/Progecto1/Form1.cs
+++ Progecto1/Progecto1/Form1.cs
@@ -12,9 +12,42 @@ namespace Progecto1
 {
     public partial class Form1 : Form
     {
+        List<Editor> ediciones;
+        TabControl tabCtrl;
         public Form1()
         {
+            ediciones =  new List<Editor>();
             InitializeComponent();
+            
+        }
+
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            }
+            catch
+            {
+                tabCtrl = new TabControl();
+                TabPage tabEntidades = new TabPage("Entidades");
+                TabPage tabAtributos = new TabPage("Atributos");
+                TabPage tabArchivoV = new TabPage("Archivo Rec");
+                TabPage tabArchivoT = new TabPage("Archivo Texto");
+                tabEntidades.BackColor = tabAtributos.BackColor =
+                    tabArchivoV.BackColor = tabArchivoT.BackColor = Color.White;
+                tabCtrl.Alignment = TabAlignment.Left;
+                tabCtrl.Controls.Add(tabEntidades);
+                tabCtrl.Controls.Add(tabAtributos);
+                tabCtrl.Controls.Add(tabArchivoV);
+                tabCtrl.Controls.Add(tabArchivoT);
+                tabCtrl.Location = new Point(0, 24);
+                tabCtrl.Name = "Editor" + ediciones.Count;
+                tabCtrl.SelectedIndex = 0;
+                tabCtrl.Size = new Size(ClientSize.Width, ClientSize.Height );
+                
+                Controls.Add(tabCtrl);
+            }
+            
         }
     }
 }
